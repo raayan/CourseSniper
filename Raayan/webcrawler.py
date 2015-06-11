321f40541bedecd35946b863f1823da9937f701f
Raayan Pillai <raayanp01@gmail.com>
2015-06-10 21:41:12 -0400

    page_scan function is updated and works, now returns a list of tuples

--- Raayan/webcrawler.py
+++ Raayan/webcrawler.py
@@ -7,49 +7,47 @@ from time import sleep
 from bs4 import BeautifulSoup
 import re
 
+#scans a web page and returns list of tuples CRN, Course Name, Status
+def page_scan(html):
+    list = []
+    soup = BeautifulSoup(html)
+    # print(soup.prettify())
+    j = 1
+    add = "01"
+    while (soup.find(id = "rpResults_ctl" + add + "_lblTitle") != None):
+        CRN = soup.find(id = "rpResults_ctl" + add + "_lblCRN").string.strip()
+        CNum = soup.find(id = "rpResults_ctl" + add + "_lblCNum").string.strip()
+        Status = soup.find(id = "rpResults_ctl" + add + "_lblStatus").string.strip()
+        # print(CRN + " || " + CNum + " || " + Status)
+        list.append((CRN, CNum, Status))
+        if j < 10:
+            add = "0" + str(j)
+        else:
+            add = str(j)
+        j+=2;
+    return list
 
-# driver = webdriver.Firefox()
-# driver.get("https://cdcs.ur.rochester.edu/")
-# driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-# sleep(2)
-#
-# selectYear = Select(driver.find_element_by_name("ddlTerm"))
-# selectYear.select_by_index(1)
-# sleep(1)
-#
-# selectSchool = Select(driver.find_element_by_name("ddlSchool"))
-# selectSchool.select_by_index(1)
-#
-# sleep(1)
-# selectDept = Select(driver.find_element_by_name("ddlDept"))
-# selectDept.select_by_index(3)
-#
-# sleep(1)
-#
-#
-# submit = driver.find_element_by_name("btnSearchTop").click()
-#
-# driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-# sleep(2)
-# html = driver.page_source
-#
-#
-# soup = BeautifulSoup(html)
-
-soup = BeautifulSoup(open("test.html"))
-
-
+driver = webdriver.Firefox()
+driver.get("https://cdcs.ur.rochester.edu/")
+driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
 sleep(2)
-# print(soup.prettify())
 
-j = 1
-add = "01"
+#choose year
+selectYear = Select(driver.find_element_by_name("ddlTerm"))
+selectYear.select_by_index(1)
+sleep(1)
 
-while (soup.find(id = "rpResults_ctl" + add + "_lblTitle") != None):
-    print(soup.find(id = "rpResults_ctl" + add + "_lblCRN").string.strip() + " || " + soup.find(id = "rpResults_ctl" + add + "_lblCNum").string.strip()+ " || " + soup.find(id = "rpResults_ctl" + add + "_lblStatus").string.strip())
+#choose
+selectDept = Select(driver.find_element_by_name("ddlDept"))
+selectDept.select_by_index(3)
+sleep(1)
 
-    if j < 10:
-        add = "0" + str(j)
-    else:
-        add = str(j)
-    j+=2;
+
+submit = driver.find_element_by_name("btnSearchTop").click()
+
+driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+sleep(2)
+html = driver.page_source
+
+
+print(page_scan(html))
