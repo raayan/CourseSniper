81289483277cb1737f879c1400325727806cd986
kynes446 <mattlee446@gmail.com>
2015-06-11 10:27:07 -0400

    cleaned up web crawling and page scanning code.

--- Matt/webcrawler.py
+++ Matt/webcrawler.py
@@ -11,12 +11,16 @@ from selenium.webdriver.support.ui import Select
 from time import sleep
 from bs4 import BeautifulSoup
 import re
+import pymongo
+from pymongo import MongoClient
+
+
+
 
 #scans webpage and pulls courses and stores in tuple (CRN, NAME, STATUS)
 def page_scan(html):
     list = []
     soup = BeautifulSoup(html)
-    # print(soup.prettify())
     j = 1
     add = "01"
     while (soup.find(id = "rpResults_ctl" + add + "_lblTitle") != None):
@@ -24,7 +28,7 @@ def page_scan(html):
         CRN = soup.find(id = "rpResults_ctl" + add + "_lblCRN").string.strip()
         CNum = soup.find(id = "rpResults_ctl" + add + "_lblCNum").string.strip()
         Status = soup.find(id = "rpResults_ctl" + add + "_lblStatus").string.strip()
-        print(CRN + " || " + CNum + " || " + Status)
+        #print(CRN + " || " + CNum + " || " + Status)
         list.append((CRN, CNum, Status))
         j+=2;
         if j < 10:
@@ -47,42 +51,28 @@ def web_crawler():
     options = selectDept.options
 
     for i in range(1, len(options)):
-        page_scan(driver.page_source)
-        sleep(2)
+
         selectDept = Select(driver.find_element_by_name('ddlDept'))
         selectDept.select_by_index(i)
         sleep(1)
         submit = driver.find_element_by_name("btnSearchTop").click()
         driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+        sleep(2)
+        classes = page_scan(driver.page_source)
+        print(classes)
+        sleep(2)
+
+    driver.close()
+
+def addToDB(class_tuples):
+        client = MongoClient()
+        db = client.test1
+        for x in class_tuples:
+            post = {"CNum": x[0], "CRN":x[1], "Status": x[2]}
+            posts = db.posts
+            post_id = posts.insert_one(post).inserted_id
 
 
 
 
-
-#
-# driver = webdriver.Firefox()
-# driver.get("https://cdcs.ur.rochester.edu/")
-# sleep(2)
-#
-# selectYear = Select(driver.find_element_by_name("ddlTerm"))
-# selectYear.select_by_index(6)
-# sleep(1)
-#
-# selectSchool = Select(driver.find_element_by_name("ddlSchool"))
-# selectSchool.select_by_index(6)
-#
-# sleep(1)
-# selectDept = Select(driver.find_element_by_name("ddlDept"))
-# selectDept.select_by_index(1)
-#
-# sleep(1)
-#
-# submit = driver.find_element_by_name("btnSearchTop").click()
-#
-# driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-# sleep(2)
-# html = driver.page_source
-#
-# page_scan(html)
-#
 web_crawler()
