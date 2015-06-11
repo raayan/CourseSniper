b1cc230cca4b9972a9ff66aeabab5c2c32aac170
Raayan Pillai <raayanp01@gmail.com>
2015-06-10 20:30:52 -0400

    Added Raayan's end, uploaded html for testing

---
+++ Raayan/webcrawler.py
@@ -0,0 +1,43 @@
+__author__ = 'horsetamer'
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from time import sleep
+from bs4 import BeautifulSoup
+import re
+
+
+# driver = webdriver.Firefox()
+# driver.get("https://cdcs.ur.rochester.edu/")
+# driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+# sleep(2)
+#
+# selectYear = Select(driver.find_element_by_name("ddlTerm"))
+# selectYear.select_by_index(1)
+# sleep(1)
+#
+# selectSchool = Select(driver.find_element_by_name("ddlSchool"))
+# selectSchool.select_by_index(1)
+#
+# sleep(1)
+# selectDept = Select(driver.find_element_by_name("ddlDept"))
+# selectDept.select_by_index(3)
+#
+# sleep(1)
+#
+#
+# submit = driver.find_element_by_name("btnSearchTop").click()
+#
+# driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+# sleep(2)
+# html = driver.page_source
+#
+#
+# soup = BeautifulSoup(html)
+
+soup = BeautifulSoup(open("test.html"))
+
+
+sleep(2)
+print(soup.prettify())
